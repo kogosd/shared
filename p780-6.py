c03c9c2dfd9df8f86179d820496b6cde7639b972
david k <dkogos@gmail.com>
2022-01-16 21:16:17 -0500

    drawing

--- p780-6.py
+++ p780-6.py
@@ -17,6 +17,7 @@ def validate(k,n, N):
     assert(abs(cos1)<=1)
     assert(abs(cos2)<=1)
     print(f"k={k:5} n={n:5} a1={a1:.3f},b1={b1:.3f},cos1={cos1:.3f},N1={a1*b1*4/sqrt(3)}   a2={a2:.3f},b2={b2:.3f},cos2={cos2:.3f},N2={a2*b2*4/sqrt(3)}")
+    if not isclosetoany(abs(cos1), 0.5,sqrt(3)/2,0,1):
     clear()
     draw_rectangle(0,0, 0,-b1, -a1, -b1, -a1, 0)
     draw_triangles_along_line(-a1/n,0, 0, -b1/k, N)
