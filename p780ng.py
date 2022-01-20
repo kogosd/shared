ea5e53910d838333c6142a38f24cba037762b3dc
david k <dkogos@gmail.com>
2022-01-19 21:32:16 -0500

    checkpoint

--- p780ng.py
+++ p780ng.py
@@ -5,21 +5,20 @@ from cyclehelper import *
 from lev import *
 
 D = defaultdict(int)
-for k in range(1, 200):
-    for m in range(k+1, 200):
+LIMIT = 10**5
+for m in range(1, LIMIT+1):
+    for k in range(m+1, LIMIT+1):
+        if m==0: continue
         NMIN = int(sqrt(12*k**2*m**2))+1
-        if NMIN>100: continue        
+        if NMIN>LIMIT: break        
         print(f"NMIN={NMIN:5}")
-
         C = getcycle(k,m)
-        for n in range(NMIN, 200):
-            print(f"n={n} k={k} m={m} C = {C} D[n]={D[n]}")
-            if C*n % (2*k) == 0 :
-                print(f"B={C*n//2//k}")
-                D[n] += 4
+        for n in range(NMIN, LIMIT+1):
+            if (C*n) % (2*k) == 0 :
+                #print(f"n={n} k={k} m={m} C = {C} D[n]={D[n]} B={C*n/2/k}")                
+                D[n] += 8
 
 S = 0
-for n in range(101):
+for n in range(LIMIT+1):
     S += D[n] + parallel(n)
-    print(f"parallel of {n} is {parallel(n)}")
-    print(f"n={n} N={D[n]} S={S}")            
+    print(f"n={n:5} N={D[n]:5} parallel={parallel(n):5} S={S:5}")            
